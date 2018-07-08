c7d2209253348bc1b4f781962f06baabbffb3f51
Assasinnys <dmitry.zenevich@yandex.by>
2018-07-08 21:20:39 +0300

    new command

--- src/main/java/herbott/listeners/PhrasesListener.java
+++ src/main/java/herbott/listeners/PhrasesListener.java
@@ -3,8 +3,19 @@ package herbott.listeners;
 import org.pircbotx.hooks.ListenerAdapter;
 import org.pircbotx.hooks.events.MessageEvent;
 
+import java.util.Objects;
+
 public class PhrasesListener extends ListenerAdapter {
 
     @Override
-    public void onMessage(MessageEvent event) {}
+    public void onMessage(MessageEvent event) {
+        String nick = Objects.requireNonNull(event.getUser()).getNick();
+        String message = event.getMessage().toLowerCase();
+
+        if (message.matches("(.*)кусь(.*)")) {
+            event.respondChannel("КУСЬ КУСЬ КУСЬ КУСЬ");
+        } else if (message.matches("(.*)фыр(.*)")) {
+            event.respondChannel("ФЫР-ФЫР-ФЫР-ФЫР!!!");
+        }
+    }
 }
