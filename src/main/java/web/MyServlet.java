78606c021940ac6e80c26d6839ebda833b69fe4c
Assasinnys <dmitry.zenevich@yandex.by>
2018-07-14 05:23:15 +0300

    test

--- src/main/java/web/MyServlet.java
+++ src/main/java/web/MyServlet.java
@@ -7,12 +7,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = "/")
+@WebServlet(urlPatterns = "/s")
 public class MyServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         ServletOutputStream out = response.getOutputStream();
-        out.print("<h1>HI HI HI!!!");
+        out.print("<h1>HI HI HI!!!</h1>");
     }
 }
