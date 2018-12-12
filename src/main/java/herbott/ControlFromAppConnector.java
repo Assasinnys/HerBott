ae789aeb3457eb1f9f38cbdd4801a32b01c94ed8
Assasinnys <dmitry.zenevich@yandex.by>
2018-12-12 23:10:49 +0300

    test control connector

--- src/main/java/herbott/ControlFromAppConnector.java
+++ src/main/java/herbott/ControlFromAppConnector.java
@@ -1,5 +1,13 @@
 package herbott;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -12,23 +20,23 @@ public class ControlFromAppConnector extends Thread {
 
     @Override
     public void run() {
+        Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+        context.addServlet(new ServletHolder(new Hi()), "/*");
         try {
-            serverSocket = new ServerSocket(7755);
-            clientSocket = new Socket();
-            String hostAddress = clientSocket.getLocalAddress().toString();
-            System.out.println("hostAddress = " + hostAddress);
-            ipAddress = serverSocket.getInetAddress().getHostAddress();
-            System.out.println("Address socket = " + ipAddress);
-            String hostName = serverSocket.getInetAddress().getHostName();
-            System.out.println("HostName = " + hostName);
-            String canonicalHostName = serverSocket.getInetAddress().getCanonicalHostName();
-            System.out.println("canonicalHostName = " + canonicalHostName);
-            while (true) {
-                clientSocket = serverSocket.accept();
-                System.out.println("Connection complete!");
-            }
-        } catch (IOException io) {
+            server.start();
+            server.join();
+        } catch (Exception io) {
             io.printStackTrace();
+        }
+    }
+
+    class Hi extends HttpServlet {
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            System.out.println("get request accepted! ☺");
         }
     }
 }
