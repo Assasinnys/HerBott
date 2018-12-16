963703835685a9bc453ff6144d21004069f05aa3
Assasinnys <dmitry.zenevich@yandex.by>
2018-12-16 17:19:11 +0300

    test control connector

--- src/main/java/herbott/ControlFromAppConnector.java
+++ src/main/java/herbott/ControlFromAppConnector.java
@@ -9,14 +9,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
 
 public class ControlFromAppConnector extends Thread {
 
-    private ServerSocket serverSocket;
-    private Socket clientSocket;
-    private volatile String ipAddress;
 
     @Override
     public void run() {
