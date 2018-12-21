ffa4f2507d0475ef73b0312e846231252517f088
Assasinnys <dmitry.zenevich@yandex.by>
2018-12-22 01:02:40 +0300

    test index.html

--- src/main/java/herbott/webserver/servlets/Hi.java
+++ src/main/java/herbott/webserver/servlets/Hi.java
@@ -7,8 +7,22 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class Hi extends HttpServlet {
+
+    private String index = "<!DOCTYPE html>\n" +
+            "<html>\n" +
+            "  <head>\n" +
+            "    <title>Заголовок</title>\n" +
+            "  </head>\n" +
+            "  <body>\n" +
+            "    <h1> ААААААААААААААААААААААААБ</h1>\n" +
+            "  </body>\n" +
+            "</html>";
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("GET request accepted! ☺");
+        resp.getOutputStream().print(index);
+        resp.getOutputStream().flush();
+        resp.setStatus(HttpServletResponse.SC_OK);
     }
 }
