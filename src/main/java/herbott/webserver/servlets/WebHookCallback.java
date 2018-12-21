46320910eaaaacaf9ec9f97fdca26ad683fad8f7
Assasinnys <dmitry.zenevich@yandex.by>
2018-12-21 22:01:37 +0300

    update subscription callback

--- src/main/java/herbott/webserver/servlets/WebHookCallback.java
+++ src/main/java/herbott/webserver/servlets/WebHookCallback.java
@@ -15,6 +15,8 @@ import java.util.Map;
 public class WebHookCallback extends HttpServlet{
 
     private String challenge = null;
+    private static final int HTTP_OK = 200;
+    private static final int HTTP_BAD_REQUEST = 400;
     private static final String HUB_CHALLENGE = "hub.challenge";
     private static final String HUB_MODE = "hub.mode";
     private static final String DATA = "data";
@@ -23,16 +25,22 @@ public class WebHookCallback extends HttpServlet{
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Map<String, String[]> params = req.getParameterMap();
         System.out.println("get callback accepted");
-        if (params.containsKey(HUB_CHALLENGE)) {
-            System.out.println("sub accepted");
-            challenge = params.get(HUB_CHALLENGE)[0];
-            resp.getOutputStream().print(challenge);
-            resp.getOutputStream().flush();
-            resp.setStatus(200);
-        } else if (params.containsKey(HUB_MODE)) {
-            if (params.get(HUB_MODE)[0].equalsIgnoreCase("denied")) {
+
+        if (params.containsKey(HUB_MODE)) {
+            String hubMode = params.get(HUB_MODE)[0];
+            if (hubMode.equalsIgnoreCase("denied")) {
                 System.out.println("sub denied");
-                resp.setStatus(200);
+                resp.setStatus(HTTP_OK);
+            } else if (hubMode.equalsIgnoreCase("subscribe")) {
+                if (params.containsKey(HUB_CHALLENGE)) {
+                    System.out.println("sub accepted");
+                    challenge = params.get(HUB_CHALLENGE)[0];
+                    resp.getOutputStream().print(challenge);
+                    resp.getOutputStream().flush();
+                    resp.setStatus(HTTP_OK);
+                } else {
+                    resp.setStatus(HTTP_BAD_REQUEST);
+                }
             }
         } else {
             try {
