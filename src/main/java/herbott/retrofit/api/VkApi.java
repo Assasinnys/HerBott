17c2f60e4ac8918fd3243eee3f3843303a1cce16
Assasinnys <dmitry.zenevich@yandex.by>
2019-02-04 14:07:02 +0300

    test vk wall.post

--- src/main/java/herbott/retrofit/api/VkApi.java
+++ src/main/java/herbott/retrofit/api/VkApi.java
@@ -8,7 +8,9 @@ import retrofit2.http.Query;
 
 public interface VkApi {
 
-    @GET("wall.post?from_group=1&signed=0&v=5.92&guid=stream&access_token=" + Main.VK_ACCESS_TOKEN)
+    // guid=stream&
+
+    @GET("wall.post?from_group=1&signed=0&v=5.92&access_token=" + Main.VK_ACCESS_TOKEN)
     Call<ResponseBody> postStreamNotice(@Query("owner_id") int ownerId, @Query("from_group") int fromGroup,
                                         @Query("message") String message);
 }
